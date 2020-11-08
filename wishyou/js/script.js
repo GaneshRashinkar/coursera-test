066ad88852738ee2f8c1cc71ccbb3d1019151656
GaneshRashinkar <ganeshrashinkar007@gmail.com>
2020-11-08 15:45:39 -0500

    added bomb

--- wishyou/js/script.js
+++ wishyou/js/script.js
@@ -17,7 +17,8 @@ final_text=txt.replace(" ","-");
 else{
 final_text=txt;
 }
-a="https://ganeshrashinkar.github.io/coursera-test/wishyou/Dhantrayodashi1.html?";
+/*a="https://ganeshrashinkar.github.io/coursera-test/wishyou/Dhantrayodashi1.html?";*/
+a="file:///C:/Users/Apex/coursera-test/WishYou/Dhantrayodashi1.html?";
 
 var c="!"
 window.open(a+final_text+c);
@@ -61,4 +62,14 @@ else{
 	var wa="whatsapp://send?text=https%3A%2F%2F";
 	/*alert(wa+final_text);*/
 	window.open(wa+final_text);
+}
+function boom(){
+	document.getElementById("bomb").style.opacity="0";
+	document.getElementById("boo").style.opacity="100";
+	document.getElementById("boo").style.animation="dhad 1s 1";
+	document.getElementById("boo").style.animation="gayab 1s 1";
+	document.getElementById("bo").style.animation="gayab 1s 1";
+	document.getElementById("boo").style.animationFillMode="forwards";
+	document.getElementById("bo").style.animationFillMode="forwards";
+		
 }
